e4f892ff780789147be2ff9fb1e7bc4112b112d3
Zujeily <zujeily@gmail.com>
2022-09-17 15:29:33 -0600

    Avance semana 10 Zujeily

---
+++ src/test/java/com/Tienda_Clase_AppWebYPatrones/TiendaClaseAppWebYPatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_Clase_AppWebYPatrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaClaseAppWebYPatronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
