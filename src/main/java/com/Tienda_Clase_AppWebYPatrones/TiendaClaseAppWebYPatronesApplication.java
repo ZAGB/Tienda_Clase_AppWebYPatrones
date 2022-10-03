60125cd46341c7a87df65e219c82beb779907f5b
barranz <barranz@SJO-5CG1236LS5.ant.amazon.com>
2022-10-03 17:12:16 -0600

    Actualizacion semana 03

---
+++ src/main/java/com/Tienda_Clase_AppWebYPatrones/TiendaClaseAppWebYPatronesApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_Clase_AppWebYPatrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaClaseAppWebYPatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaClaseAppWebYPatronesApplication.class, args);
+	}
+
+}
